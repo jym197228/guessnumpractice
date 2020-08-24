5325951e47f6f9fedcbabff0795ab26b6e78cc0f
Chris Chung <jym197@gmail.com>
2020-08-24 14:20:25 +0800

    add counting function

--- r.py
+++ r.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('請猜猜數字: ')
 	num = int(num)
 	if num == r:
 		print('您猜中了!')
+		print('您總共猜了', count, '次')
 		break
 	elif num > r:
 		print('答案比您輸入的還要小')
 	else:
 		print('答案比您輸入的還要大')
+	print('您猜了', count, '次')
